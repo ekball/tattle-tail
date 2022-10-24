16a610fdb2ead4c3800ca14749de3c827a05453b
ekball <90882418+ekball@users.noreply.github.com>
2022-10-24 13:16:29 -0400

    commented out the Commands tab for the moment until bug is fixed, added info to the sit page

--- src/components/Sit/index.js
+++ src/components/Sit/index.js
@@ -10,9 +10,28 @@ function Sit() {
       <div className='flex justify-evenly align-center p-5'>
 
       <p className='flex flex-wrap w-1/2'>
-            Hello, my name is Kyles and I am currently a dog trainer at Petco. I have been training dogs for about 4 years now. I graduated from UNC-Chapel Hill in 2017 with a B.A. in German Studies and a minor in Linguistics. 
-            Coding has always been an interest of mine, but I did not have the opportunity to learn it in a formal environment until I enrolled in the UNC Coding Bootcamp in Fall 2021. 
-            I would like to find a position that revolves around web development or creating independent projects for people.
+        Start off in a low distraction environment. We are NOT going to use a verbal cue initially.
+              
+        Put a smelly treat in the palm of your hand and hold in place with your thumb.
+
+        Allow dog to sniff the treat, but do not give it to them.
+
+        Move the lure/treat slowly over the dog’s head, high enough that he must crane his neck back to con,nue targe,ng, but not so high that he jumps to target.
+
+        Move the lure slightly backward over the dog while maintaining the same height.
+
+        Now wait!
+
+        If the dog gets up or jumps, start over.
+
+        When the dog is signg, mark the behavior and give the reward.
+
+        Repeat this 4 - 6 times and each time, the dog should sit with less and less effort from you.
+      </p>
+
+      <p>
+      Another way to think about is that there is a string attached from your middle finger to the dog’s nose. 
+      As you bring your hand up in front of the dog, their head will follow up while their butt hits the ground.
       </p>
 
 
